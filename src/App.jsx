56edb293e67ac7faa0d43daacc598049e3aa13f1
Stephen Anderson <sa3196a@student.american.edu>
2017-09-01 14:41:30 -0700

    Create store

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Segment, Container } from 'semantic-ui-react';
-import Spreadsheet from './components/Spreadsheet.js';
+import Spreadsheet from './Components/Spreadsheet.js';
 
 const App = () => (
   <div className="App">
