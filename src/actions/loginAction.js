ee4a77121ef74c4de9453ad48e4cce26629c8022
Ronan Fitzpatrick <xronanfitzx@gmail.com>
2017-09-13 15:55:22 -0700

    Create login action and reducer

---
+++ src/actions/loginAction.js
@@ -0,0 +1,7 @@
+export const RENDER_LOGIN = 'RENDER_LOGIN';
+
+export const renderLogin = () => ({
+  type: RENDER_LOGIN,
+});
+
+// in component using mapDisptachToProps dispatch the above action creator
