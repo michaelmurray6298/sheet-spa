e722810104851ff4ed7a5b56f077fb8443b547d5
Michael Murray <michaelmurray6298@gmail.com>
2017-09-18 16:28:18 -0700

    Export reducer

--- src/store.js
+++ src/store.js
@@ -1,15 +1,12 @@
 import { createStore, applyMiddleware } from 'redux';
 import thunkMiddleware from 'redux-thunk';
-import logger from 'redux-logger';
 import rootReducer from './reducers/index';
 import SHEETS_API from './utils/Api';
 /* eslint-disable no-underscore-dangle */
 
 const store = createStore(
   rootReducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
   applyMiddleware(thunkMiddleware.withExtraArgument({ SHEETS_API })),
-  logger,
 );
 
 
