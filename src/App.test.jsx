a92b2460ecc984442169bb2e3d68ae725867aa28
Grant Willison <emailgdw@gmail.com>
2017-09-18 12:51:47 -0700

    Write Snapshot test

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,8 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+/* eslint-disable import/extensions */
+import React from 'react';
+import toJson from 'enzyme-to-json';
+import { shallow } from 'enzyme';
+import configureStore from 'redux-mock-store';
+import thunkMiddleware from 'redux-thunk';
+import { Provider } from 'react-redux';
+import SHEET_API from './utils/Api';
+import App from './App.jsx';
+
+const middleware = [ thunkMiddleware.withExtraArgument({ SHEET_API })];
+const mockStore = configureStore(middleware);
+describe('App component', () => {
+  it('should render a component with props as specified ', () => {
+    const component = shallow(
+      <Provider store={mockStore({})}>
+        <App />
+      </Provider>,
+    );
+    expect(toJson(component)).toMatchSnapshot();
+  });
 });
